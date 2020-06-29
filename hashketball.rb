2551fc809fdd4d03c2cd0ee78e858a1602a97fec
Micah Mosley <micah.mosley@utexas.edu>
2020-06-29 19:46:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
 # Write your code here!
 
 def game_hash
-  hash={home: {team_name:"", colors: [], players:[{player_name: "Alan Anderson", number: "0", shoe:"16", }, {}]}, away: {}}
+  hash={home: {team_name:"", colors: [], players:[{player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks:1}, ]}, away: {}}
